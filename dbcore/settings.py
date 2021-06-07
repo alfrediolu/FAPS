8cdfe3c19be548f69042d4d0f03050ed1631625b
Lucas Wilken <lucas.wilken@ndsu.edu>
2021-06-07 11:21:18 -0500

    Added Postgres connection.

--- dbcore/settings.py
+++ dbcore/settings.py
@@ -70,6 +70,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+dbHeroku = dj_database_url.config(conn_max_age = 600)
+DATABASES['default'].update(dbHeroku)
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
