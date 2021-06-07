e46b29f15d9718539172f66d351c4b069753f1b6
Lucas Wilken <lucas.wilken@ndsu.edu>
2021-06-07 12:51:20 -0500

    Search testing

--- proteindb/views.py
+++ proteindb/views.py
@@ -3,12 +3,11 @@ from django.shortcuts import render
 from . models import uniProtein, simProtein
 from django.views.generic import TemplateView, ListView
 from django.db.models import Q
-from itertools import chain
 
 class index(TemplateView):
     template_name = "index.html"
 
-class searchResults(ListView):
+class uniSearchResults(ListView):
     model = uniProtein
     context_object_name = 'uniList'
     template_name = "searchResults.html"
@@ -18,7 +17,7 @@ class searchResults(ListView):
         uniList = uniProtein.objects.filter(Q(accession__icontains = query))
         return uniList
 
-class searchResults(ListView):
+class simSearchResults(ListView):
     model = simProtein
     context_object_name = 'simList'
     template_name = "searchResults.html"
