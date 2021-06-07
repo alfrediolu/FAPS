c231701214059140b1d44034a7e8ade29bc43fc5
Lucas Wilken <lucas.wilken@ndsu.edu>
2021-06-07 12:39:17 -0500

    Search testing

--- proteindb/views.py
+++ proteindb/views.py
@@ -1,7 +1,9 @@
+from typing import ChainMap
 from django.shortcuts import render
 from . models import uniProtein, simProtein
 from django.views.generic import TemplateView, ListView
 from django.db.models import Q
+from itertools import chain
 
 class index(TemplateView):
     template_name = "index.html"
@@ -14,4 +16,5 @@ class searchResults(ListView):
         query = self.request.GET.get('q')
         simList = simProtein.objects.filter(Q(accession__icontains = query))
         uniList = uniProtein.objects.filter(Q(accession__icontains = query))
-        return uniList
+        object_list = list(chain(simList, uniList))
+        return object_list
