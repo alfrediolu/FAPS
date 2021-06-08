c7762fee33d59bb48aa0f1a083ae551f938bb728
Lucas Wilken <lucas.wilken@ndsu.edu>
2021-06-08 13:03:52 -0500

    Working on query system.

--- proteindb/views.py
+++ proteindb/views.py
@@ -8,19 +8,16 @@ class index(TemplateView):
 
 class searchResults(ListView):
     template_name = "searchResults.html"
+    context_object_name = 'uni_list'
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
         context['query'] = self.request.GET.get('q')
+        query = self.request.GET.get('q')
+        context['sim_list'] = simProtein.simManage.search(query).order_by('accession')
         return context
 
     def get_queryset(self):
         query = self.request.GET.get('q')
-        
         uniResults = uniProtein.uniManage.search(query).order_by('accession')
-        simResults = simProtein.simManage.search(query).order_by('accession')
-        if len(simResults) == 0 and len(uniResults) == 0:
-            qs = None
-            return qs
-        qs = list(chain(uniResults, simResults))
-        return qs
+        return uniResults
