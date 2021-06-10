7f57c461ef4c2cc54b5be93d5351fd9d82e544f2
Lucas Wilken <lucas.wilken@ndsu.edu>
2021-06-10 10:16:23 -0500

    New requirements for dataframe/csv processing installed.

--- proteindb/views.py
+++ proteindb/views.py
@@ -3,6 +3,7 @@ from . models import uniProtein, simProtein
 from django.views.generic import TemplateView, ListView
 from itertools import chain
 import csv, re
+import pandas as pd
 from django.contrib import messages
 
 class index(TemplateView):
@@ -23,47 +24,16 @@ class searchResults(ListView):
         uniResults = uniProtein.uniManage.search(query).order_by('accession')
         return uniResults
 
-def csvUpload(request):
-    if request.method == 'POST':
-           uploadedCSV = request.FILES['uploadedCSV']
-    return render(request, 'csvSearch.html')
+class csvUploader(TemplateView):
+    template_name = "csvSearch.html"
 
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        return context
 
-# def csvUpload(request):
-#     if request.method == 'POST':
-#         uploadedCSV = request.FILES['uploadedCSV']
-#         accessionList = []
-
-#         if not uploadedCSV.name.endswith('.csv'):
-#             print("Invalid file uploaded")
-#             return
-
-#         else:
-#             dataSet = uploadedCSV.read().decode('UTF-8')
-#             csvReader = csv.reader(dataSet, delimiter = ',')
-#             headers = []
-#             for row in csvReader:
-#                 headers.append(row)
-#                 break
-
-#             accessionCol = 0
-#             validCSV = False
-
-#             for colName in headers:
-#                 colName = str(colName)
-#                 if re.search('accession', colName, re.IGNORECASE):
-#                     validCSV = True
-#                 else:
-#                     accessionCol += 1
-
-#         if not validCSV:
-#             print("Invalid CSV uploaded")
-#             return
-
-#         for rows in csvReader:
-#             currentAccession = csvReader[accessionCol]
-#             accessionList.append(currentAccession)
-
-#         print(accessionList)
-            
-#     return render(request, 'csvSearch.html')
+    def csvUpload(request):
+        if request.POST and request.FILES:
+            uploadedCSV = request.FILES['uploadedCSV']
+            readCSV = pd.read_csv(uploadedCSV, delimiter = ',')
+            accessionList = readCSV['Accession']
+        return render(request, 'csvSearch.html')
