8ca00bc4083484ee6f6782344091842d042793bc
Lucas Wilken <lucas.wilken@ndsu.edu>
2021-06-11 10:50:48 -0500

    Testing upload feature.

--- proteindb/views.py
+++ proteindb/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render
 from . models import uniProtein, simProtein
 from django.views.generic import TemplateView, ListView, FormView, CreateView, View
+from django.shortcuts import redirect
 import pandas as pd
 from . handlers import accessionGrabber
 
 
 class index(TemplateView):
     template_name = "index.html"
+    accessionList = []
 
     def csvUpload(self, request):
         if request.POST and request.FILES:
             uploadedCSV = request.FILES['uploadedCSV']
             if uploadedCSV.is_valid():
-                accessionGrabber(uploadedCSV)
-        return render(request, 'csvSearch.html')
+                accessionList = accessionGrabber(uploadedCSV)
+        return redirect('csvsearch/')
 
 class searchResults(ListView):
     template_name = "searchResults.html"
@@ -30,15 +32,17 @@ class searchResults(ListView):
         uniResults = uniProtein.uniManage.search(query).order_by('accession')
         return uniResults
 
-class csvSearchResults(View):
+class csvSearchResults(ListView):
     template_name = "csvSearch.html"
+    context_object_name = 'uni_list'
 
-class singleUniprotUploader(CreateView):
-    template_name = "protUploader.html"
-    model = uniProtein
-    fields = ['Accession', 'a-Helix', 'b-Sheet', 'Turn', 'Unknown', 'Known', 'Length']
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        query = self.request.GET.get('q')
+        context['sim_list'] = simProtein.simManage.search(query).order_by('accession')
+        return context
 
-class singleSimProtUploader(CreateView):
-    template_name = "protUploader.html"
-    model = uniProtein
-    fields = ['Accession', 'simType', 'a-Helix', 'b-Sheet', 'Turn', 'Length']
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        uniResults = uniProtein.uniManage.search(query).order_by('accession')
+        return uniResults
