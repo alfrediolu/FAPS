00137628891cfc912639c35d5b5d2814019a8813
Lucas Wilken <lucas.wilken@ndsu.edu>
2021-06-11 11:19:48 -0500

    Testing upload feature.

--- proteindb/urls.py
+++ proteindb/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', index.as_view(), name = "index"),
     path('search/', searchResults.as_view(), name = "searchresults"),
     path('csvsearch/', csvSearchResults.as_view(), name = "csvsearch"),
-    path('csvsearch/invalid', csvSearchInvalid.as_vieW(), name = "csvsearchinvalid"),
+    path('csvsearch/invalid', csvSearchInvalid.as_view(), name = "csvsearchinvalid"),
 ]
