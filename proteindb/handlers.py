288914c8e76cbb8b1a01b7e9fa70497eaff3dc9b
Lucas Wilken <lucas.wilken@ndsu.edu>
2021-06-29 18:49:37 -0500

    Data handler.

--- proteindb/handlers.py
+++ proteindb/handlers.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+# This file serves as handlers to simplify the code found in views. The names should tell you what each one does from views.py, but more documentation has been added here.
+
+
 # Grabs a list of accession codes from the uploaded .csv file. Pandas is required for this, because .columns is a function of a pandas dataframe.
 def accessionGrabber(csv):
     if 'Accession' in csv.columns:
@@ -9,9 +12,11 @@ def accessionGrabber(csv):
 # Standardizes the column names of the uploaded .csv file so they can be indexed without having to search by potential matches while adding data to the db.
 def columnRename(df):
     colNames = df.columns
+    print(colNames)
     helixMatches = ["helix", "alpha", "a-helix"]
     betaMatches = ["beta", "sheet", "b-sheet"]
     turnMatches = ["turn", "random", "coil"]
+
     for name in colNames:
         nameCheck = name.lower()
         if any(colName in nameCheck for colName in helixMatches):
