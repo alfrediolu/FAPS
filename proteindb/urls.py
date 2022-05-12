1a7d0470a8a81f00bc5d6e704e6846ca3b64d5f9
Nijhum Paul <nijhum.paul@ndsu.edu>
2022-05-12 15:17:37 -0500

    urls.py

--- proteindb/urls.py
+++ proteindb/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . views import index, heLp, searchResults, csvSearchResults, csvSearchInvalid, csvSearch, upload, uploadLogin
+from . views import index, help, searchResults, csvSearchResults, csvSearchInvalid, csvSearch, upload, uploadLogin
 
 # Defines URLs for the page. All associated HTML files share the same name as the view the URL represents.
 # - index houses all the original input/search forms.
