f732f74cf8c89ef3db4a7c8e134e8b4dede63ea3
Nijhum Paul <nijhum.paul@ndsu.edu>
2022-05-13 12:32:19 -0500

    urls.py

--- dbcore/urls.py
+++ dbcore/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Represents the core URLs for the database. AdminCP is affiliated with the dbcore module and not proteindb.
 # Also registers all URLs from proteindb.
