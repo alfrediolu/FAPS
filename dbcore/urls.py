c1501145ce5e013d1f4dba24a0142e63228766f1
Nijhum Paul <nijhum.paul@ndsu.edu>
2022-05-13 12:28:41 -0500

    urls.py

--- dbcore/urls.py
+++ dbcore/urls.py
@@ -8,3 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('proteindb.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL ,document_root = settings.STATIC_ROOT)
